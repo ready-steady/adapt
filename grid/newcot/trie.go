a7dbf38518194e5af48bc1bd05ab14cb53236103
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-15 20:17:50 +0200

    Improved tap in trie

--- grid/newcot/trie.go
+++ grid/newcot/trie.go
@@ -1,8 +1,8 @@
 package newcot
 
 type trie struct {
-	ic   uint16
-	ml   uint8
+	ic   uint32
+	ml   uint32
 	root *node
 }
 
@@ -11,69 +11,151 @@ type node struct {
 	children []*node
 }
 
+func newTrie(ic uint16, ml uint8) *trie {
+	return &trie{
+		ic: uint32(ic),
+		ml: uint32(ml),
+		root: &node{
+			children: make([]*node, 0, ml+1),
+		},
+	}
+}
+
 // tap looks for the given sequence of levels and orders and returns true if
 // found or returns false if not found, in which case the sequence is appended
 // to the internal structure.
 func (t *trie) tap(levels []uint8, orders []uint32) bool {
-	var i uint16
-	var value uint32
-	var n, c *node
+	var i, j, value, count uint32
+	var k int32
+	var c, g *node
 
-	n = t.root
+	n := t.root
 
 overLevels:
 	for i = 0; i < t.ic; i++ {
 		value = uint32(levels[i])
+		count = uint32(len(n.children))
 
-		for _, c = range n.children {
+		for j = 0; j < count; j++ {
+			c = n.children[j]
 			if c.value == value {
 				n = c
 				continue overLevels
+			} else if c.value > value {
+				// The children are always kept sorted.
+				break
 			}
 		}
 
-		for ; i < t.ic; i++ {
+		// Create an order leaf.
+		g = &node{
+			value: orders[t.ic - 1],
+		}
+
+		// Create the rest of order nodes.
+		for k = int32(t.ic) - 2; k >= 0; k-- {
+			c = &node{
+				value:    orders[k],
+				children: make([]*node, 1, 1<<t.ml+1),
+			}
+			c.children[0] = g
+			g = c
+		}
+
+		if i == t.ic - 1 {
+			// Create the missing level node.
 			c = &node{
 				value:    uint32(levels[i]),
-				children: make([]*node, 0, t.ml+1),
+				children: make([]*node, 1, 1<<t.ml+1),
 			}
-			n.children = append(n.children, c)
-			n = c
+			c.children[0] = g
+		} else {
+			// Create a level leaf.
+			c = &node{
+				value:    uint32(levels[t.ic - 1]),
+				children: make([]*node, 1, 1<<t.ml+1),
+			}
+			c.children[0] = g
+			g = c
+
+			// Create the rest of level nodes down to i exclusive.
+			for k = int32(t.ic) - 2; k > int32(i); k-- {
+				c = &node{
+					value:    uint32(levels[k]),
+					children: make([]*node, 1, t.ml+1),
+				}
+				c.children[0] = g
+				g = c
+			}
+
+			// Create the missing level node.
+			c = &node{
+				value:    uint32(levels[i]),
+				children: make([]*node, 1, t.ml+1),
+			}
+			c.children[0] = g
 		}
+
+		// Insert the node at the jth position.
+		n.children = n.children[:count+1]
+		copy(n.children[j+1:], n.children[j:count])
+		n.children[j] = c
+
+		return false
 	}
 
 overOrders:
 	for i = 0; i < t.ic; i++ {
 		value = orders[i]
+		count = uint32(len(n.children))
 
-		for _, c = range n.children {
+		for j = 0; j < count; j++ {
+			c = n.children[j]
 			if c.value == value {
 				n = c
 				continue overOrders
+			} else if c.value > value {
+				// The children are always kept sorted.
+				break
 			}
 		}
 
-		for ; i < t.ic; i++ {
+		if i == t.ic - 1 {
+			// Create the missing order node.
+			c = &node{
+				value: orders[i],
+			}
+		} else {
+			// Create an order leaf.
+			g = &node{
+				value: orders[t.ic - 1],
+			}
+
+			// Create the rest of order nodes up to i exclusive.
+			for k = int32(t.ic) - 2; k > int32(i); k-- {
+				c = &node{
+					value:    orders[k],
+					children: make([]*node, 1, 1<<t.ml+1),
+				}
+				c.children[0] = g
+				g = c
+			}
+
+			// Create the missing order node.
 			c = &node{
 				value:    orders[i],
-				children: make([]*node, 0, 1<<t.ml+1),
+				children: make([]*node, 1, 1<<t.ml+1),
 			}
-			n.children = append(n.children, c)
-			n = c
+			c.children[0] = g
 		}
+
+		// Insert the node at the jth position.
+		n.children = n.children[:count+1]
+		copy(n.children[j+1:], n.children[j:count])
+		n.children[j] = c
 
 		return false
 	}
 
 	return true
-}
-
-func newTrie(ic uint16, ml uint8) *trie {
-	return &trie{
-		ic: ic,
-		ml: ml,
-		root: &node{
-			children: make([]*node, 0, ml+1),
-		},
-	}
 }
--- grid/newcot/trie_test.go
+++ grid/newcot/trie_test.go
@@ -7,12 +7,7 @@ import (
 )
 
 func TestTrieTap(t *testing.T) {
-	const (
-		dimensions = 3
-		maxLevel   = 1
-	)
-
-	trie := newTrie(dimensions, maxLevel)
+	trie := newTrie(3, 1)
 
 	assert.Equal(trie.tap([]uint8{0, 0, 0}, []uint32{0, 1, 2}), false, t)
 	assert.Equal(trie.tap([]uint8{0, 0, 0}, []uint32{0, 1, 2}), true, t)
@@ -53,4 +48,18 @@ func TestTrieTap(t *testing.T) {
 	n = n.children[0]
 	assert.Equal(n.value, uint32(2), t)
 	assert.Equal(len(n.children), 0, t)
+}
+
+
+func TestTrieTap1D(t *testing.T) {
+	t.Skip()
+	trie := newTrie(1, 4)
+
+	levels := []uint8{1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4}
+	orders := []uint32{0, 2, 1, 3, 1, 3, 5, 7, 1, 3, 5, 7, 9, 11, 13, 15}
+
+	for i := range levels {
+		assert.Equal(trie.tap(levels[i:i+1], orders[i:i+1]), false, t)
+		assert.Equal(trie.tap(levels[i:i+1], orders[i:i+1]), true, t)
+	}
 }
