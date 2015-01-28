3994718ac116bd4a321bf41b2422b42622796fea
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-01-28 10:55:35 +0100

    Added benchmarks for Interpolant.Evaluate

--- interpolation/adhier/main_test.go
+++ interpolation/adhier/main_test.go
@@ -2,6 +2,7 @@ package adhier
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/ready-steady/numeric/basis/linhat"
@@ -11,61 +12,47 @@ import (
 
 func TestComputeStep(t *testing.T) {
 	interpolator := prepare(&fixtureStep)
-
 	surrogate := interpolator.Compute(step)
-
 	assert.Equal(surrogate, fixtureStep.surrogate, t)
 }
 
 func TestEvaluateStep(t *testing.T) {
 	interpolator := prepare(&fixtureStep)
-
 	values := interpolator.Evaluate(fixtureStep.surrogate, fixtureStep.points)
-
 	assert.Equal(values, fixtureStep.values, t)
 }
 
 func TestComputeHat(t *testing.T) {
 	interpolator := prepare(&fixtureHat)
-
 	surrogate := interpolator.Compute(hat)
-
 	assert.Equal(surrogate, fixtureHat.surrogate, t)
 }
 
 func TestEvaluateHat(t *testing.T) {
 	interpolator := prepare(&fixtureHat)
-
 	values := interpolator.Evaluate(fixtureHat.surrogate, fixtureHat.points)
-
 	assert.AlmostEqual(values, fixtureHat.values, t)
 }
 
 func TestComputeCube(t *testing.T) {
 	interpolator := prepare(&fixtureCube)
-
 	surrogate := interpolator.Compute(cube)
-
 	assert.Equal(surrogate, fixtureCube.surrogate, t)
 }
 
 func TestComputeBox(t *testing.T) {
 	interpolator := prepare(&fixtureBox)
-
 	surrogate := interpolator.Compute(box)
-
 	assert.Equal(surrogate, fixtureBox.surrogate, t)
 }
 
 func TestEvaluateBox(t *testing.T) {
 	interpolator := prepare(&fixtureBox)
-
 	values := interpolator.Evaluate(fixtureBox.surrogate, fixtureBox.points)
-
 	assert.AlmostEqual(values, fixtureBox.values, t)
 }
 
-func BenchmarkHat(b *testing.B) {
+func BenchmarkComputeHat(b *testing.B) {
 	interpolator := prepare(&fixtureHat)
 
 	for i := 0; i < b.N; i++ {
@@ -73,7 +60,7 @@ func BenchmarkHat(b *testing.B) {
 	}
 }
 
-func BenchmarkCube(b *testing.B) {
+func BenchmarkComputeCube(b *testing.B) {
 	interpolator := prepare(&fixtureCube, func(config *Config) {
 		config.MaxLevel = 9
 	})
@@ -83,7 +70,7 @@ func BenchmarkCube(b *testing.B) {
 	}
 }
 
-func BenchmarkBox(b *testing.B) {
+func BenchmarkComputeBox(b *testing.B) {
 	interpolator := prepare(&fixtureBox, func(config *Config) {
 		config.MaxLevel = 9
 	})
@@ -93,7 +80,7 @@ func BenchmarkBox(b *testing.B) {
 	}
 }
 
-func BenchmarkMany(b *testing.B) {
+func BenchmarkComputeMany(b *testing.B) {
 	const (
 		inputs  = 2
 		outputs = 1000
@@ -106,11 +93,74 @@ func BenchmarkMany(b *testing.B) {
 			oc:    outputs,
 		},
 	})
-
 	function := many(inputs, outputs)
 
 	for i := 0; i < b.N; i++ {
 		interpolator.Compute(function)
+	}
+}
+
+func BenchmarkEvaluateHat(b *testing.B) {
+	interpolator := prepare(&fixtureHat)
+	surrogate := interpolator.Compute(hat)
+	points := generate(surrogate)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		interpolator.Evaluate(surrogate, points)
+	}
+}
+
+func BenchmarkEvaluateCube(b *testing.B) {
+	interpolator := prepare(&fixtureCube, func(config *Config) {
+		config.MaxLevel = 9
+	})
+	surrogate := interpolator.Compute(cube)
+	points := generate(surrogate)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		interpolator.Evaluate(surrogate, points)
+	}
+}
+
+func BenchmarkEvaluateBox(b *testing.B) {
+	interpolator := prepare(&fixtureBox, func(config *Config) {
+		config.MaxLevel = 9
+	})
+	surrogate := interpolator.Compute(box)
+	points := generate(surrogate)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		interpolator.Evaluate(surrogate, points)
+	}
+}
+
+func BenchmarkEvaluateMany(b *testing.B) {
+	const (
+		inputs  = 2
+		outputs = 1000
+	)
+
+	interpolator := prepare(&fixture{
+		surrogate: &Surrogate{
+			level: 9,
+			ic:    inputs,
+			oc:    outputs,
+		},
+	})
+	function := many(inputs, outputs)
+	surrogate := interpolator.Compute(function)
+	points := generate(surrogate)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		interpolator.Evaluate(surrogate, points)
 	}
 }
 
@@ -214,4 +264,18 @@ func prepare(fixture *fixture, arguments ...interface{}) *Interpolator {
 	interpolator, _ := New(newcot.NewClosed(ic), linhat.NewClosed(ic, oc), config, oc)
 
 	return interpolator
+}
+
+func generate(surrogate *Surrogate) []float64 {
+	const (
+		count = 10000
+	)
+
+	generator := rand.New(rand.NewSource(0))
+	points := make([]float64, count * surrogate.ic)
+	for i := range points {
+		points[i] = generator.Float64()
+	}
+
+	return points
 }
