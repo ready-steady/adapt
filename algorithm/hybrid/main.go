5da0b52508bc6617efac7f8a51edcac395d54d1f
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-12-16 19:25:39 +0100

    a/hybrid: add Basis, Grid, and Interpolator

--- algorithm/hybrid/main.go
+++ algorithm/hybrid/main.go
@@ -1,3 +1,21 @@
 // Package hybrid provides an algorithm for hierarchical interpolation with
 // hybrid adaptation.
 package hybrid
+
+// Basis is a functional basis.
+type Basis interface {
+	// Compute evaluates the value of a basis function.
+	Compute([]uint64, []float64) float64
+}
+
+// Grid is a sparse grid.
+type Grid interface {
+	// Compute returns the nodes corresponding to a set of indices.
+	Compute([]uint64) []float64
+}
+
+// Interpolator is an instance of the algorithm.
+type Interpolator struct {
+	basis Basis
+	grid  Grid
+}
