65e50e4393fff75063f6e1d0251d1645f4ff0661
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-02-09 16:03:29 +0100

    b/polynomial: decompose Compute and Integrate

--- basis/polynomial/closed.go
+++ basis/polynomial/closed.go
@@ -17,91 +17,91 @@ func NewClosed(dimensions uint, order uint) *Closed {
 
 // Compute evaluates a basis function.
 func (self *Closed) Compute(index []uint64, point []float64) float64 {
-	nd, np := self.nd, uint64(self.np)
-
-	value := 1.0
-	for i := uint(0); i < nd; i++ {
-		level, power := levelMask&index[i], np
-		if level < power {
-			power = level
-		}
-		if power == 0 {
-			continue // value *= 1.0
-		}
-
-		order := index[i] >> levelSize
-		xi, step := closedNode(level, order)
-
-		x := point[i]
-		delta := math.Abs(x - xi)
-		if delta >= step {
-			return 0.0 // value *= 0.0
-		}
-
-		if power == 1 {
-			// Use two linear segments. The reason is that there are three
-			// points (including the endpoints), but a first-order polynomial
-			// can satisfy only two of them in general.
-			value *= 1.0 - delta/step
-			continue
-		}
-
-		// The left endpoint of the local support.
-		xl := xi - step
-		value *= (x - xl) / (xi - xl)
-		power -= 1
-
-		// The right endpoint of the local support.
-		xr := xi + step
-		value *= (x - xr) / (xi - xr)
-		power -= 1
-
-		// Find the rest of the needed ancestors.
-		for power > 0 {
-			level, order = closedParent(level, order)
-			xj, _ := closedNode(level, order)
-			if equal(xj, xl) || equal(xj, xr) {
-				continue
-			}
-			value *= (x - xj) / (xi - xj)
-			power -= 1
-		}
+	nd, np, value := self.nd, self.np, 1.0
+	for i := uint(0); i < nd && value != 0.0; i++ {
+		value *= closedCompute(levelMask&index[i], index[i]>>levelSize, np, point[i])
 	}
-
 	return value
 }
 
 // Integrate computes the integral of a basis function.
 func (self *Closed) Integrate(index []uint64) float64 {
-	nd, np := self.nd, uint64(self.np)
+	nd, np, value := self.nd, self.np, 1.0
+	for i := uint(0); i < nd && value != 0.0; i++ {
+		value *= closedIntegrate(levelMask&index[i], index[i]>>levelSize, np)
+	}
+	return value
+}
+
+func closedCompute(level, order uint64, power uint, x float64) float64 {
+	if level < uint64(power) {
+		power = uint(level)
+	}
+	if power == 0 {
+		return 1.0
+	}
+
+	xi, step := closedNode(level, order)
+
+	delta := math.Abs(x - xi)
+	if delta >= step {
+		return 0.0
+	}
+
+	if power == 1 {
+		// Use two linear segments. The reason is that there are three points
+		// (including the endpoints), but a first-order polynomial can satisfy
+		// only two of them in general.
+		return 1.0 - delta/step
+	}
 
 	value := 1.0
-	for i := uint(0); i < nd; i++ {
-		level, power := levelMask&index[i], np
-		if level < power {
-			power = level
-		}
-		if power == 0 {
-			continue // value *= 1.0
-		}
 
-		order := index[i] >> levelSize
-		_, step := closedNode(level, order)
+	// The left endpoint of the local support.
+	xl := xi - step
+	value *= (x - xl) / (xi - xl)
+	power -= 1
 
-		if power == 1 {
-			// Use two liner segments. See the corresponding comment in Compute.
-			if level == 1 {
-				value *= 0.25
-			} else {
-				value *= step
-			}
+	// The right endpoint of the local support.
+	xr := xi + step
+	value *= (x - xr) / (xi - xr)
+	power -= 1
+
+	// Find the rest of the needed ancestors.
+	for power > 0 {
+		level, order = closedParent(level, order)
+		xj, _ := closedNode(level, order)
+		if equal(xj, xl) || equal(xj, xr) {
 			continue
 		}
-
-		panic("not implemented")
+		value *= (x - xj) / (xi - xj)
+		power -= 1
 	}
 
 	return value
+}
+
+func closedIntegrate(level, order uint64, power uint) float64 {
+	if level < uint64(power) {
+		power = uint(level)
+	}
+	if power == 0 {
+		return 1.0
+	}
+
+	_, step := closedNode(level, order)
+
+	if power == 1 {
+		// Use two liner segments. See the corresponding comment in
+		// computeClosed.
+		if level == 1 {
+			return 0.25
+		} else {
+			return step
+		}
+	}
+
+	panic("not implemented")
 }
 
 func closedNode(level, order uint64) (x, step float64) {
