8088821e9f1454dba8661a913eebaefd8d93b750
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-02-16 08:29:33 +0100

    b/polynomial: simplify the scaling of quadrature rules

--- basis/polynomial/support.go
+++ basis/polynomial/support.go
@@ -15,7 +15,7 @@ func getRule(order uint) *rule {
 	if rule, ok := ruleCache[order]; ok {
 		return rule
 	}
-	x, w := quadrature.Legendre(order, -1.0, 1.0)
+	x, w := quadrature.Legendre(order, 0.0, 1.0)
 	rule := &rule{x, w}
 	ruleCache[order] = rule
 	return rule
@@ -24,8 +24,8 @@ func getRule(order uint) *rule {
 func integrate(a, b float64, order uint, target func(float64) float64) float64 {
 	value, rule := 0.0, getRule(order)
 	for i := uint(0); i < order; i++ {
-		x := ((a + b) + (b-a)*rule.x[i]) / 2.0
-		w := (b - a) * rule.w[i] / 2.0
+		x := a + (b-a)*rule.x[i]
+		w := (b - a) * rule.w[i]
 		value += w * target(x)
 	}
 	return value
