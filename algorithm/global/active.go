879d510bf3dd0a62d323f5f3aab8279cd409862b
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-02-25 11:14:49 +0100

    a/global: eliminate Norms

--- algorithm/global/active.go
+++ algorithm/global/active.go
@@ -9,7 +9,6 @@ import (
 type Active struct {
 	external.Active
 
-	Norms  []uint64  // Norms of level indices
 	Scores []float64 // Scores of level indices
 
 	k    uint
@@ -25,25 +24,16 @@ func newActive(ni uint, config *Config) *Active {
 		ni:   ni,
 		rate: config.AdaptivityRate,
 
-		Norms:  []uint64{1},
 		Scores: []float64{0.0},
 	}
 }
 
 func (self *Active) pull() []uint64 {
-	k := internal.LocateMinUint64s(self.Norms, self.Positions)
-	min, max := self.Norms[k], internal.MaxUint64s(self.Norms)
-	if float64(min) > (1.0-self.rate)*float64(max) {
-		k = internal.LocateMaxFloat64s(self.Scores, self.Positions)
-	}
-
-	self.k = k
-	norm := self.Norms[k] + 1
-	indices := self.Advance(k)
+	self.k = internal.LocateMaxFloat64s(self.Scores, self.Positions)
+	indices := self.Advance(self.k)
 
 	nn := uint(len(indices)) / self.ni
 	for i := uint(0); i < nn; i++ {
-		self.Norms = append(self.Norms, norm)
 		self.Scores = append(self.Scores, infinity)
 	}
 
