1c2254b791d0cde7a6cee1ced6305ef49872bc47
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-02-25 08:28:12 +0100

    a/global: export Norms and Scores

--- algorithm/global/active.go
+++ algorithm/global/active.go
@@ -9,12 +9,14 @@ import (
 type Active struct {
 	external.Active
 
+	// Norms contains the norms of the indices.
+	Norms []uint64
+	// Scores contains the scores of the indices.
+	Scores []float64
+
 	k    uint
 	ni   uint
 	rate float64
-
-	norms  []uint64
-	scores []float64
 }
 
 func newActive(ni uint, config *Config) *Active {
@@ -25,30 +27,32 @@ func newActive(ni uint, config *Config) *Active {
 		ni:   ni,
 		rate: config.AdaptivityRate,
 
-		norms: []uint64{1},
+		Norms:  []uint64{1},
+		Scores: []float64{0.0},
 	}
 }
 
 func (self *Active) pull() []uint64 {
-	k := internal.LocateMinUint64s(self.norms, self.Positions)
-	min, max := self.norms[k], internal.MaxUint64s(self.norms)
+	k := internal.LocateMinUint64s(self.Norms, self.Positions)
+	min, max := self.Norms[k], internal.MaxUint64s(self.Norms)
 	if float64(min) > (1.0-self.rate)*float64(max) {
-		k = internal.LocateMaxFloat64s(self.scores, self.Positions)
+		k = internal.LocateMaxFloat64s(self.Scores, self.Positions)
 	}
 
 	self.k = k
-	norm := self.norms[k] + 1
+	norm := self.Norms[k] + 1
 	indices := self.Advance(k)
 
 	nn := uint(len(indices)) / self.ni
 	for i := uint(0); i < nn; i++ {
-		self.norms = append(self.norms, norm)
+		self.Norms = append(self.Norms, norm)
+		self.Scores = append(self.Scores, 0.0)
 	}
 
 	return indices
 }
 
 func (self *Active) push(scores []float64) {
+	copy(self.Scores[len(self.Scores)-len(scores):], scores)
 	self.Forget(self.k)
-	self.scores = append(self.scores, scores...)
 }
