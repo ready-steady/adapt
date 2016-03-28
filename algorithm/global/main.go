56168523a3ca97f3d05274aaf8ccc5ec075a74e1
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-03-28 12:39:10 +0200

    a/{global,hibrid}: remove support.go

--- algorithm/global/main.go
+++ algorithm/global/main.go
@@ -106,3 +106,21 @@ func (self *Interpolator) Evaluate(surrogate *external.Surrogate, points []float
 	return internal.Approximate(self.basis, surrogate.Indices, surrogate.Surpluses, points,
 		surrogate.Inputs, surrogate.Outputs, internal.Workers)
 }
+
+func score(strategy Strategy, state *State, ni, no uint) []float64 {
+	nn := uint(len(state.Counts))
+	scores := make([]float64, nn)
+	for i, o := uint(0), uint(0); i < nn; i++ {
+		count := state.Counts[i]
+		element := Element{
+			Lindex:       state.Lindices[i*ni : (i+1)*ni],
+			Indices:      state.Indices[o*ni : (o+count)*ni],
+			Volumes:      state.Volumes[o:(o + count)],
+			Observations: state.Observations[o*no : (o+count)*no],
+			Surpluses:    state.Surpluses[o*no : (o+count)*no],
+		}
+		scores[i] = strategy.Score(&element)
+		o += count
+	}
+	return scores
+}
