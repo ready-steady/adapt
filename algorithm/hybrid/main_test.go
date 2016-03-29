a6059b4b8a769fdac12fbc8aebbe27f3be979fe1
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-03-29 11:38:21 +0200

    a/hybrid: test admissibility and uniqueness

--- algorithm/hybrid/main_test.go
+++ algorithm/hybrid/main_test.go
@@ -3,6 +3,7 @@ package hybrid
 import (
 	"testing"
 
+	"github.com/ready-steady/adapt/algorithm/internal"
 	"github.com/ready-steady/assert"
 )
 
@@ -11,6 +12,8 @@ func TestBranin(t *testing.T) {
 	interpolator := prepare(fixture)
 
 	surrogate := interpolator.Compute(fixture.target)
+	assert.Equal(internal.IsUnique(surrogate.Indices, surrogate.Inputs), true, t)
+	assert.Equal(internal.IsAdmissible(surrogate.Indices, surrogate.Inputs), true, t)
 
 	values := interpolator.Evaluate(surrogate, fixture.points)
 	assert.EqualWithin(values, fixture.values, 100.0, t)
