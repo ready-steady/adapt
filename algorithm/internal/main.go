99a4918b0a3d478e9515b5a0263a9563ed8c8974
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-04-11 18:20:30 +0200

    a/internal: make a cosmetic adjustment

--- algorithm/internal/main.go
+++ algorithm/internal/main.go
@@ -98,26 +98,26 @@ func Index(indexer GridIndexer, lindices []uint64, ni uint) ([]uint64, []uint) {
 	return indices, counts
 }
 
-// Invoke evaluates a function at multiple nodes using multiple goroutines.
-func Invoke(compute func([]float64, []float64), nodes []float64, ni, no uint) []float64 {
-	nn := uint(len(nodes)) / ni
+// Invoke evaluates a function at multiple points using multiple goroutines.
+func Invoke(compute func([]float64, []float64), points []float64, ni, no uint) []float64 {
+	np := uint(len(points)) / ni
 
-	values := make([]float64, nn*no)
+	values := make([]float64, np*no)
 
-	jobs := make(chan uint, nn)
+	jobs := make(chan uint, np)
 	group := sync.WaitGroup{}
-	group.Add(int(nn))
+	group.Add(int(np))
 
 	for i := uint(0); i < Workers; i++ {
 		go func() {
 			for j := range jobs {
-				compute(nodes[j*ni:(j+1)*ni], values[j*no:(j+1)*no])
+				compute(points[j*ni:(j+1)*ni], values[j*no:(j+1)*no])
 				group.Done()
 			}
 		}()
 	}
 
-	for i := uint(0); i < nn; i++ {
+	for i := uint(0); i < np; i++ {
 		jobs <- i
 	}
 
