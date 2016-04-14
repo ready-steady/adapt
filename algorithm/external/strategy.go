58357ae7a0f917e57cc9f61f9af9d7f2db51e319
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-04-14 10:30:34 +0200

    a/external: move structs around

--- algorithm/external/strategy.go
+++ algorithm/external/strategy.go
@@ -1,5 +1,28 @@
 package external
 
+// Element contains information about an interpolation element.
+type Element struct {
+	Index []uint64 // Nodal index
+
+	Volume  float64   // Basis-function volume
+	Value   []float64 // Target-function value
+	Surplus []float64 // Hierarchical surplus
+}
+
+// State contains information about an interpolation iteration.
+type State struct {
+	Lindices []uint64 // Level indices
+	Indices  []uint64 // Nodal indices
+	Counts   []uint   // Number of nodal indices for each level index
+
+	Nodes     []float64 // Grid nodes
+	Volumes   []float64 // Basis-function volumes
+	Values    []float64 // Target-function values
+	Estimates []float64 // Approximated values
+	Surpluses []float64 // Hierarchical surpluses
+	Scores    []float64 // Nodal-index scores
+}
+
 // Strategy controls the interpolation process.
 type Strategy interface {
 	// First returns the initial state of the first iteration.
